f162b1490fffc2a6435832f907b63c1150b1d1c0
Fransuelton <Elton6103@gmail.com>
2023-08-30 20:45:49 -0300

    feat: add functionality to mark all messages as read

--- src/components/HeaderArea/index.jsx
+++ src/components/HeaderArea/index.jsx
@@ -1,58 +1,56 @@
-import { styled } from "styled-components"
+import { styled } from "styled-components";
 
-
-const HeaderArea = () => {
-
-    return (
-        <Header>
-            <Div>
-                <H1>Notifications</H1>
-                <P>3</P>
-            </Div>
-            <A>Mark all as read</A>
-        </Header>
-    )
-}
+const HeaderArea = ({ handleUnreadClick, messageCount }) => {
+  return (
+    <Header>
+      <Div>
+        <H1>Notifications</H1>
+        <P>{messageCount}</P>
+      </Div>
+      <A onClick={handleUnreadClick}>Mark all as read</A>
+    </Header>
+  );
+};
 
 const Header = styled.header`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: 100%;
-    margin: 2.4rem 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  margin: 2.4rem 0;
 
-    @media (min-width: 1200px) and (max-width: 1919px) {
-        margin: 3.1rem 0;
-    }
-`
+  @media (min-width: 1200px) and (max-width: 1919px) {
+    margin: 3.1rem 0;
+  }
+`;
 
 const Div = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const H1 = styled.h1`
-    font-size: 2rem;
-    font-weight: 800;
-    color: #1C202B;
+  font-size: 2rem;
+  font-weight: 800;
+  color: #1c202b;
 
-    @media (min-width: 1200px) and (max-width: 1919px) {
-        font-size: 2.4rem;
-    }
-`
+  @media (min-width: 1200px) and (max-width: 1919px) {
+    font-size: 2.4rem;
+  }
+`;
 
 const P = styled.p`
-    background-color: #0A327B;
-    border-radius: 0.6rem;
-    width: 3.2rem;
-    height: 2.5rem;
-    margin-left: 0.9rem;
-    color: #FFF;
-    font-size: 1.6rem;
-    font-weight: 800;
-    text-align: center;
-`
+  background-color: #0a327b;
+  border-radius: 0.6rem;
+  width: 3.2rem;
+  height: 2.5rem;
+  margin-left: 0.9rem;
+  color: #fff;
+  font-size: 1.6rem;
+  font-weight: 800;
+  text-align: center;
+`;
 
 const A = styled.a`
     color: #5E6778;
@@ -81,6 +79,6 @@ const A = styled.a`
         color: #0A327B;
         text-decoration: underline;
     }
-`
+`;
 
-export { HeaderArea }
+export { HeaderArea };
--- src/components/NotificationArea/index.jsx
+++ src/components/NotificationArea/index.jsx
@@ -1,226 +1,252 @@
-import { styled } from "styled-components"
-import { HeaderArea } from "../HeaderArea"
-import AvatarMax from '../../assets/images/avatar-mark-webber.webp'
-import AvatarAngela from '../../assets/images/avatar-angela-gray.webp'
-import AvatarJacob from '../../assets/images/avatar-jacob-thompson.webp'
-import AvatarRizky from '../../assets/images/avatar-rizky-hasanuddin.webp'
-import AvatarKimberly from '../../assets/images/avatar-kimberly-smith.webp'
-import AvatarNathan from '../../assets/images/avatar-nathan-peterson.webp'
-import AvatarAnna from '../../assets/images/avatar-anna-kim.webp'
-import ChessePhoto from '../../assets/images/image-chess.webp'
+import React, { useState } from "react";
+import { styled } from "styled-components";
+import { HeaderArea } from "../HeaderArea";
+import AvatarMax from "../../assets/images/avatar-mark-webber.webp";
+import AvatarAngela from "../../assets/images/avatar-angela-gray.webp";
+import AvatarJacob from "../../assets/images/avatar-jacob-thompson.webp";
+import AvatarRizky from "../../assets/images/avatar-rizky-hasanuddin.webp";
+import AvatarKimberly from "../../assets/images/avatar-kimberly-smith.webp";
+import AvatarNathan from "../../assets/images/avatar-nathan-peterson.webp";
+import AvatarAnna from "../../assets/images/avatar-anna-kim.webp";
+import ChessePhoto from "../../assets/images/image-chess.webp";
 
 const NotificationArea = () => {
-    return (
-        <NotificationsList>
-            <HeaderArea />
-            <Section>
-                <Img src={AvatarMax} alt="Foto de Perfil" />
-                <Info>
-                    <Description>
-                        <UserName>Mark Webberr</UserName> reacted to your recent post <ReactedNotification>My first tournament today!<Unread /></ReactedNotification>
-                    </Description>
+  const [unreadVisible, setUnreadVisible] = useState(true);
+  const [messageCount, setMessageCount] = useState(3);
 
-                    <NotificationTime>1m ago</NotificationTime>
-                </Info>
-            </Section>
+  const handleUnreadClick = () => {
+    setUnreadVisible(false);
+    setMessageCount(0);
+  };
 
-            <Section>
-                <Img src={AvatarAngela} alt="Foto de Perfil" />
-                <Info>
-                    <Description>
-                        <UserName>Angela Gray</UserName> followed you<Unread />
-                    </Description>
+  return (
+    <NotificationsList>
+      <HeaderArea
+        handleUnreadClick={handleUnreadClick}
+        messageCount={messageCount}
+      />
+      <Section>
+        <Img src={AvatarMax} alt="Foto de Perfil" />
+        <Info>
+          <Description>
+            <UserName>Mark Webberr</UserName> reacted to your recent post{" "}
+            <ReactedNotification>
+              My first tournament today!
+              {unreadVisible && <Unread />}
+            </ReactedNotification>
+          </Description>
 
-                    <NotificationTime>5m ago</NotificationTime>
-                </Info>
-            </Section>
+          <NotificationTime>1m ago</NotificationTime>
+        </Info>
+      </Section>
 
-            <Section>
-                <Img src={AvatarJacob} alt="Foto de Perfil" />
-                <Info>
-                    <Description>
-                        <UserName>Jacob Thompson</UserName> has joined your group <GroupNotification>Chess Club</GroupNotification><Unread />
-                    </Description>
+      <Section>
+        <Img src={AvatarAngela} alt="Foto de Perfil" />
+        <Info>
+          <Description>
+            <UserName>Angela Gray</UserName> followed you
+            {unreadVisible && <Unread />}
+          </Description>
 
-                    <NotificationTime>1 day ago</NotificationTime>
-                </Info>
-            </Section>
+          <NotificationTime>5m ago</NotificationTime>
+        </Info>
+      </Section>
 
-            <Section>
-                <Img src={AvatarRizky} alt="Foto de Perfil" />
-                <Info>
-                    <Description>
-                        <UserName>Rizky Hasanuddin</UserName> sent you a private message
-                    </Description>
+      <Section>
+        <Img src={AvatarJacob} alt="Foto de Perfil" />
+        <Info>
+          <Description>
+            <UserName>Jacob Thompson</UserName> has joined your group{" "}
+            <GroupNotification>Chess Club</GroupNotification>
+            {unreadVisible && <Unread />}
+          </Description>
 
-                    <NotificationTime>5 days ago</NotificationTime>
+          <NotificationTime>1 day ago</NotificationTime>
+        </Info>
+      </Section>
 
-                    
-                    <PrivateMessage>Hello, thanks for setting up the Chess Club. I’ve been a member for a few weeks now and I’m already having lots of fun and improving my game.</PrivateMessage>
-                </Info>
-            </Section>
+      <Section>
+        <Img src={AvatarRizky} alt="Foto de Perfil" />
+        <Info>
+          <Description>
+            <UserName>Rizky Hasanuddin</UserName> sent you a private message
+          </Description>
 
-            <Section>
-                <Img src={AvatarKimberly} alt="Foto de Perfil" />
-                <Info>
-                    <Description>
-                        <UserName>Kimberly Smith</UserName> commented on your picture
-                    </Description>
+          <NotificationTime>5 days ago</NotificationTime>
 
-                    <NotificationTime>1 week ago</NotificationTime>
-                </Info>
-                <ChesseImg src={ChessePhoto} alt="Foto de uma menina jogando xadrez" />
-            </Section>
+          <PrivateMessage>
+            Hello, thanks for setting up the Chess Club. I’ve been a member for
+            a few weeks now and I’m already having lots of fun and improving my
+            game.
+          </PrivateMessage>
+        </Info>
+      </Section>
 
-            <Section>
-                <Img src={AvatarNathan} alt="Foto de Perfil" />
-                <Info>
-                    <Description>
-                        <UserName>Nathan Peterson</UserName> reacted to your recent post <ReactedNotification>5 end-game strategies to increase your win rate</ReactedNotification>
-                    </Description>
+      <Section>
+        <Img src={AvatarKimberly} alt="Foto de Perfil" />
+        <Info>
+          <Description>
+            <UserName>Kimberly Smith</UserName> commented on your picture
+          </Description>
 
-                    <NotificationTime>2 weeks ago</NotificationTime>
-                </Info>
-            </Section>
+          <NotificationTime>1 week ago</NotificationTime>
+        </Info>
+        <ChesseImg src={ChessePhoto} alt="Foto de uma menina jogando xadrez" />
+      </Section>
 
-            <Section>
-                <Img src={AvatarAnna} alt="Foto de Perfil" />
-                <Info>
-                    <Description>
-                        <UserName>Anna Kim</UserName> left the group <GroupNotification>Chess Club</GroupNotification>
-                    </Description>
+      <Section>
+        <Img src={AvatarNathan} alt="Foto de Perfil" />
+        <Info>
+          <Description>
+            <UserName>Nathan Peterson</UserName> reacted to your recent post{" "}
+            <ReactedNotification>
+              5 end-game strategies to increase your win rate
+            </ReactedNotification>
+          </Description>
 
-                    <NotificationTime>2 weeks ago</NotificationTime>
-                </Info>
-            </Section>
-        </NotificationsList>
-    );
-}
+          <NotificationTime>2 weeks ago</NotificationTime>
+        </Info>
+      </Section>
+
+      <Section>
+        <Img src={AvatarAnna} alt="Foto de Perfil" />
+        <Info>
+          <Description>
+            <UserName>Anna Kim</UserName> left the group{" "}
+            <GroupNotification>Chess Club</GroupNotification>
+          </Description>
+
+          <NotificationTime>2 weeks ago</NotificationTime>
+        </Info>
+      </Section>
+    </NotificationsList>
+  );
+};
 
 const NotificationsList = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-    margin: 0 1.6rem 4rem 1.6rem;
-`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin: 0 1.6rem 4rem 1.6rem;
+`;
 
 const Section = styled.section`
-    display: flex;
-    background-color: #F7FAFD;
-    min-width: 34.3rem;
-    max-width: 67rem;
-    padding: 1.6rem;
-    border-radius: 0.8rem;
-    gap: 1rem;
-`
+  display: flex;
+  background-color: #f7fafd;
+  min-width: 34.3rem;
+  max-width: 67rem;
+  padding: 1.6rem;
+  border-radius: 0.8rem;
+  gap: 1rem;
+`;
 
 const Info = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items-center;
-`
+`;
 
 const Img = styled.img`
-    width: 3.9rem;
-    height: 3.9rem;
-    border-radius: 3.9rem;
-    border: 1px solid rgba(0, 0, 0, 0.05);
-    margin-right: 1.3rem;
+  width: 3.9rem;
+  height: 3.9rem;
+  border-radius: 3.9rem;
+  border: 1px solid rgba(0, 0, 0, 0.05);
+  margin-right: 1.3rem;
 
-    @media (min-width: 1200px) and (max-width: 1919px) {
-        width: 4.5rem;
-        height: 4.5rem;
-        border-radius: 4.5rem;
-    }
-`
+  @media (min-width: 1200px) and (max-width: 1919px) {
+    width: 4.5rem;
+    height: 4.5rem;
+    border-radius: 4.5rem;
+  }
+`;
 
 const ChesseImg = styled.img`
-    cursor: pointer;
-    width: 3.9rem;
-    height: 3.9rem;
-    border-radius: 0.7rem;
+  cursor: pointer;
+  width: 3.9rem;
+  height: 3.9rem;
+  border-radius: 0.7rem;
 
-    @media (min-width: 1200px) and (max-width: 1919px) {
-        width: 4.5rem;
-        height: 4.5rem;
-        margin-left: 17rem;
-    }
-`
+  @media (min-width: 1200px) and (max-width: 1919px) {
+    width: 4.5rem;
+    height: 4.5rem;
+    margin-left: 17rem;
+  }
+`;
 
 const UserName = styled.span`
-    cursor: pointer;
-    font-size: 1.4rem;
-    font-weight: 800;
-    color: #1C202B;
-    margin-right: 0.6rem;
+  cursor: pointer;
+  font-size: 1.4rem;
+  font-weight: 800;
+  color: #1c202b;
+  margin-right: 0.6rem;
 
-    &:hover {
-        color: #0A327B;
-    }
+  &:hover {
+    color: #0a327b;
+  }
 
-    @media (min-width: 1200px) and (max-width: 1919px) {
-        font-size: 1.6rem;
-    }
-`
+  @media (min-width: 1200px) and (max-width: 1919px) {
+    font-size: 1.6rem;
+  }
+`;
 
 const Description = styled.p`
-    font-size: 1.4rem;
-    font-weight: 500;
-    color: #5E6778;
-    margin-bottom: 0.3rem;
+  font-size: 1.4rem;
+  font-weight: 500;
+  color: #5e6778;
+  margin-bottom: 0.3rem;
 
-    @media (min-width: 1200px) and (max-width: 1919px) {
-        font-size: 1.6rem;
-        margin-bottom: 0.4rem
-    }
-`
+  @media (min-width: 1200px) and (max-width: 1919px) {
+    font-size: 1.6rem;
+    margin-bottom: 0.4rem;
+  }
+`;
 
 const NotificationTime = styled.p`
-    font-size: 1.4rem;
-    font-weight: 500;
-    color: #939CAD;
+  font-size: 1.4rem;
+  font-weight: 500;
+  color: #939cad;
 
-    @media (min-width: 1200px) and (max-width: 1919px) {
-        font-size: 1.6rem;
-    }
-`
+  @media (min-width: 1200px) and (max-width: 1919px) {
+    font-size: 1.6rem;
+  }
+`;
 
 const GroupNotification = styled.span`
-    cursor: pointer;
-    color: #0A327B;
-    font-size: 14px;
-    font-weight: 700;
+  cursor: pointer;
+  color: #0a327b;
+  font-size: 14px;
+  font-weight: 700;
 
-    @media (min-width: 1200px) and (max-width: 1919px) {
-        font-size: 1.6rem;
-    }
-`
+  @media (min-width: 1200px) and (max-width: 1919px) {
+    font-size: 1.6rem;
+  }
+`;
 
 const ReactedNotification = styled.span`
-    cursor: pointer;
-    color: #5E6778;
-    font-size: 14px;
-    font-weight: 700;
+  cursor: pointer;
+  color: #5e6778;
+  font-size: 14px;
+  font-weight: 700;
 
-    &:hover {
-        color: #0A327B;
-    }
+  &:hover {
+    color: #0a327b;
+  }
 
-    @media (min-width: 1200px) and (max-width: 1919px) {
-        font-size: 1.6rem;
-        font-weight: 800;
-    }
-`
+  @media (min-width: 1200px) and (max-width: 1919px) {
+    font-size: 1.6rem;
+    font-weight: 800;
+  }
+`;
 
 const Unread = styled.span`
-    width: 8px;
-    height: 8px;
-    background-color: #F65552;
-    border-radius: 50%;
-    display: inline-block;
-    margin-left: 0.6rem;
-`
+  width: 8px;
+  height: 8px;
+  background-color: #f65552;
+  border-radius: 50%;
+  display: inline-block;
+  margin-left: 0.6rem;
+`;
 
 const PrivateMessage = styled.p`
     cursor: pointer;
@@ -239,6 +265,6 @@ const PrivateMessage = styled.p`
         border: 1px solid var(--7-very-light-grey-blue, #DDE7EE);
         background: var(--6-light-grey-blue, #E5EFFA);
     }
-`
+`;
 
-export { NotificationArea }
+export { NotificationArea };
